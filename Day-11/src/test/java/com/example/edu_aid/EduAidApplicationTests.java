14ec2c0ab4eba48b0e7d5f47b3781ff3ef8a308a
sabarivasan212003 <118351728+sabarivasan212003@users.noreply.github.com>
2024-02-09 11:54:37 +0530

    Add files via upload
    commit

---
+++ Day-11/src/test/java/com/example/edu_aid/EduAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edu_aid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
